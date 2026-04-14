d9ebe2b97b0a3409913cb1e2ce7f23ad53cce7af
kshitijkatara34-code <kshitijkatara34@gmail.com>
2026-04-14 14:59:16 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { GLMRLanding } from './components/GLMRLanding';
+
+export default function App() {
+  return (
+    <div className="w-full min-h-screen bg-[#050505]">
+      <GLMRLanding />
+    </div>
+  );
+}
